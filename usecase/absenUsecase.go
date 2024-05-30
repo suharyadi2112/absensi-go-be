36a825c2cf076bf0ffa1b91cb03ad235fbff1643
suharyadi <suharyadi21@outlook.com>
2024-05-30 10:06:55 +0700

    - create post absen handler - usecase

--- usecase/absenUsecase.go
+++ usecase/absenUsecase.go
@@ -5,32 +5,58 @@ import (
 	"fmt"
 )
 
-type AbsenTopUseCase interface {
-	GetAbsenTopUsecase(date string) ([]map[string]interface{}, error)
+var controller *cont.Conn
+
+func init() {
+	var err error
+	controller, err = cont.NewCon()
+	if err != nil {
+		panic(err) // Handle error appropriately
+	}
 }
 
-// Handler untuk endpoint /users
+// use case absen top
 func GetAbsenTopUsecase(tanggalhariIni string) ([]map[string]interface{}, error) {
 
 	fmt.Println("Tanggal sekarang - get absen top:", tanggalhariIni)
 
-	handler, err := cont.NewCon()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return nil, err
-	}
+	result := controller.GetAbsenTopQuery(tanggalhariIni)
 
-	result := handler.GetAbsenTopQuery(tanggalhariIni)
-	rowsData := result.DataAbsen
-	err = result.Err
-
-	if err != nil {
-		return nil, err
+	if result.Err != nil {
+		return nil, result.Err
 	}
 
 	//custom return yang diperlukan response
 	var absentopResp []map[string]interface{}
-	for _, s := range rowsData {
+	for _, s := range result.DataAbsen {
+		absentopResp = append(absentopResp, map[string]interface{}{
+			"IDAbsensi":  s.ID.Int64,
+			"FotoSiswa":  s.IDSiswa.Foto.String,
+			"FotoGuru":   s.IDPengajar.Foto.String,
+			"NamaSiswa":  s.IDSiswa.NamaLengkap.String,
+			"NamaGuru":   s.IDPengajar.NamaLengkap.String,
+			"Kelas":      s.IDKelas.Kelas.String,
+			"IDPengajar": s.IDPengajar.ID.Int64,
+		})
+	}
+
+	return absentopResp, nil
+}
+
+// // use case simpan tap scan
+func PostAbsenTopUsecase(formCode string) ([]map[string]interface{}, error) {
+
+	fmt.Println("Form code - post absen:", formCode)
+
+	result := controller.PostAbsenTopQuery(formCode)
+
+	if result.Err != nil {
+		return nil, result.Err
+	}
+
+	//custom return yang diperlukan response
+	var absentopResp []map[string]interface{}
+	for _, s := range result.DataAbsen {
 		absentopResp = append(absentopResp, map[string]interface{}{
 			"IDAbsensi":  s.ID.Int64,
 			"FotoSiswa":  s.IDSiswa.Foto.String,
