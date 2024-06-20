872810419e4d8d0295f7ae98ad5e0c68ba62f9d6
suharyadi <suharyadi21@outlook.com>
2024-06-20 09:33:08 +0700

    log main

--- server/main.go
+++ server/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"absensi/routes"
 
+	// conFig "absensi/config"
+
 	"github.com/labstack/echo/v4"
+	// "github.com/sirupsen/logrus"
 )
 
+// var (
+// 	logger *logrus.Logger
+// )
+
+// func init() {
+// 	logger = conFig.InitLogRus()
+// }
+
 func main() {
+
+	// ctx := "RunningServer"
 	e := echo.New()
 
 	routes.InitRoutes(e)
 
+	// conFig.InitlogError(logger, ctx, "server running at - localhost:1323", nil, "info") // catat log
 	e.Logger.Fatal(e.Start("localhost:1323"))
 	// e.Logger.Fatal(e.Start("192.168.0.244:1323"))
-
 }
