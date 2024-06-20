86b4aa34a266976c6149fb3cce1a7d7c7ca21885
suharyadi <suharyadi21@outlook.com>
2024-06-20 10:51:29 +0700

    fixing log

--- server/main.go
+++ server/main.go
@@ -3,28 +3,31 @@ package main
 import (
 	"absensi/routes"
 
-	// conFig "absensi/config"
+	conFig "absensi/config"
 
 	"github.com/labstack/echo/v4"
-	// "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
-// var (
-// 	logger *logrus.Logger
-// )
+var (
+	logger *logrus.Logger
+)
 
-// func init() {
-// 	logger = conFig.InitLogRus()
-// }
+func init() {
+	logger = conFig.InitLogRus()
+}
 
 func main() {
 
-	// ctx := "RunningServer"
+	ctx := "RunningServer"
 	e := echo.New()
 
 	routes.InitRoutes(e)
 
-	// conFig.InitlogError(logger, ctx, "server running at - localhost:1323", nil, "info") // catat log
+	conFig.InitlogError(logger, ctx, "server running at - localhost:1323", nil, "info") // catat log
 	e.Logger.Fatal(e.Start("localhost:1323"))
 	// e.Logger.Fatal(e.Start("192.168.0.244:1323"))
+
+	logrus.Info("Server run at localhost:1323")
+
 }
