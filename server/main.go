385facd68e39951de13eadb9a121b63adf7c537b
suharyadi <suharyadi21@outlook.com>
2025-01-24 00:37:26 +0700

    - handling cors

--- server/main.go
+++ server/main.go
@@ -6,6 +6,7 @@ import (
 	conFig "absensi/config"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/sirupsen/logrus"
 )
 
@@ -21,6 +22,16 @@ func main() {
 
 	ctx := "RunningServer"
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{
+			"http://103.175.216.178:5173",
+			"http://localhost:5173",
+		},
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
 
 	routes.InitRoutes(e)
 
